1927e26d3cb5c057cfc3d840301daaa72c420e98
Yale Kaul <yalestar@gmail.com>
2011-04-05 09:37:14 -0700

    removed junk, added db dump

---
+++ dbdump.sql
@@ -0,0 +1,47 @@
+CREATE TABLE "subdivisions" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "first_part" varchar(255), "second_part" varchar(255), "third_part" varchar(255), "image_path" varchar(255), "created_at" datetime, "updated_at" datetime);
+INSERT INTO "subdivisions" VALUES (1, 'Sundance', 'Terrace', 'Estates', 'sd1.jpg
+', '2011-04-05 15:53:13.883336', '2011-04-05 15:53:13.883336');
+INSERT INTO "subdivisions" VALUES (2, 'Sunset', 'Village', 'at RidgeGate', 'sd2.jpg
+', '2011-04-05 15:53:13.967101', '2011-04-05 15:53:13.967101');
+INSERT INTO "subdivisions" VALUES (3, 'Elk', 'Estates', 'at The Greens', 'sd3.jpg
+', '2011-04-05 15:53:13.970630', '2011-04-05 15:53:13.970630');
+INSERT INTO "subdivisions" VALUES (4, 'Wolf', 'Glen', 'at Wolf Hollow', 'sd4.jpg
+', '2011-04-05 15:53:13.974645', '2011-04-05 15:53:13.974645');
+INSERT INTO "subdivisions" VALUES (5, 'Marmot', 'Preserve', 'at Fox Hollow', 'sd5.jpg
+', '2011-04-05 15:53:13.978143', '2011-04-05 15:53:13.978143');
+INSERT INTO "subdivisions" VALUES (6, 'Willow', 'Woods', '', 'sd6.jpg
+', '2011-04-05 15:53:13.981485', '2011-04-05 15:53:13.981485');
+INSERT INTO "subdivisions" VALUES (7, 'Sandstone', 'Promenade', '', 'sd7.jpg
+', '2011-04-05 15:53:13.985604', '2011-04-05 15:53:13.985604');
+INSERT INTO "subdivisions" VALUES (8, 'Orchard', 'Prairie', 'at Pine Acres', 'sd8.jpg
+', '2011-04-05 15:53:13.989069', '2011-04-05 15:53:13.989069');
+INSERT INTO "subdivisions" VALUES (9, 'Aspen', 'Reserve', '', 'sd1.jpg
+', '2011-04-05 15:53:13.992403', '2011-04-05 15:53:13.992403');
+INSERT INTO "subdivisions" VALUES (10, 'Tamarac', 'Park', '', 'sd2.jpg
+', '2011-04-05 15:53:13.996458', '2011-04-05 15:53:13.996458');
+INSERT INTO "subdivisions" VALUES (11, 'Eagle', 'Meadow', '', 'sd3.jpg
+', '2011-04-05 15:53:13.999954', '2011-04-05 15:53:13.999954');
+INSERT INTO "subdivisions" VALUES (12, 'Palomino', 'Grove', '', 'sd4.jpg
+', '2011-04-05 15:53:14.002837', '2011-04-05 15:53:14.002837');
+INSERT INTO "subdivisions" VALUES (13, 'Raccoon', 'Ranch', '', 'sd6.jpg
+', '2011-04-05 15:53:14.007669', '2011-04-05 15:53:14.007669');
+INSERT INTO "subdivisions" VALUES (14, 'Fox', 'Hollow', '', 'sd5.jpg
+', '2011-04-05 15:53:14.010892', '2011-04-05 15:53:14.010892');
+INSERT INTO "subdivisions" VALUES (15, 'Bear', 'Run', '', 'sd7.jpg
+', '2011-04-05 15:53:14.013863', '2011-04-05 15:53:14.013863');
+INSERT INTO "subdivisions" VALUES (16, 'Summit', 'Creek', '', 'sd8.jpg
+', '2011-04-05 15:53:14.017017', '2011-04-05 15:53:14.017017');
+INSERT INTO "subdivisions" VALUES (17, 'Chardonnay', 'Homestead', 'at SkyRidge', 'sd1.jpg
+', '2011-04-05 15:53:14.021366', '2011-04-05 15:53:14.021366');
+INSERT INTO "subdivisions" VALUES (18, 'Lake', 'Acres', '', 'sd2.jpg
+', '2011-04-05 15:53:14.024246', '2011-04-05 15:53:14.024246');
+INSERT INTO "subdivisions" VALUES (19, 'Redstone', 'Ridge', '', 'sd3.jpg
+', '2011-04-05 15:53:14.027878', '2011-04-05 15:53:14.027878');
+INSERT INTO "subdivisions" VALUES (20, 'Coyote', 'Run', '', 'sd4.jpg
+', '2011-04-05 15:53:14.031358', '2011-04-05 15:53:14.031358');
+INSERT INTO "subdivisions" VALUES (21, 'Buffalo', 'Village', '', 'sd8.jpg
+', '2011-04-05 15:53:14.038611', '2011-04-05 15:53:14.038611');
+INSERT INTO "subdivisions" VALUES (22, 'Falcon', 'Greens', '', 'sd5.jpg
+', '2011-04-05 15:53:14.042045', '2011-04-05 15:53:14.042045');
+INSERT INTO "subdivisions" VALUES (23, 'Heritage', 'Heights', '', 'sd8.jpg
+', '2011-04-05 15:53:14.045984', '2011-04-05 15:53:14.045984');
